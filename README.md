43e509ae6e43e0cd93fe8006ca5d68c2a79ffc2a
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-05-11 08:50:05 +0100

    Updated version number.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# libChEBIp
-libChEBIp: a Python API for accessing the ChEBI database
+# libChEBIpy
+libChEBIpy: a Python API for accessing the ChEBI database
 
 Details of the API are available: http://libchebi.github.io/libChEBI%20API.pdf
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages, setup
 
 
 setup(name='libChEBIpy',
-      version='1.0.5',
+      version='1.0.6',
       description='libChEBIpy: a Python API for accessing the ChEBI database',
       long_description='libChEBIpy: a Python API for accessing the ChEBI ' +
       'database',
